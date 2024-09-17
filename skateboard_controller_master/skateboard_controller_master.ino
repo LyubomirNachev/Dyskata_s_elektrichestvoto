c1f6206192ded57cd1aa2a656ca386bfa063f795
Lyubomir Nachev <48567009+LyubomirNachev@users.noreply.github.com>
2024-09-17 03:45:49 +0300

    Working version
    Working logic, configured as ble server, can reconnect if there is premature disconnect.

--- skateboard_controller_master/skateboard_controller_master.ino
+++ skateboard_controller_master/skateboard_controller_master.ino
@@ -1,99 +1,165 @@
-/*
-  Program to control LED (ON/OFF) from ESP32 using Serial Bluetooth
-  by Daniel Carrasco -> https://www.electrosoftcloud.com/
-*/
-#include "BluetoothSerial.h" // We will include the Serial Bluetooth header
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-BluetoothSerial BT; // Bluetooth Object
-String clientName = "ESP32_Skateboard";
-bool connected;
-int value;
-int flag = 0;
-int flag_stop = 0;
-int flag_dir = 0;
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include <BLE2901.h>
+
+#define SERVICE_UUID        "63b5a895-bd3d-4d34-b360-607d92fb7f13"
+#define BRAKE_CHAR_UUID     "38ee12c0-b3ff-40ec-a7fa-0c0307ad7032"
+#define STOP_CHAR_UUID      "fb8cfd3d-260a-4e90-a55c-2ab854de8d0a"
+#define DIR_CHAR_UUID       "4a5d63a1-1fae-493d-bf72-2b923b0756ee"
+#define SPEED_CHAR_UUID     "81010060-39d8-494e-a11c-f071fa06fbbb"
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristicBrake = NULL;
+BLECharacteristic *pCharacteristicStop = NULL;
+BLECharacteristic *pCharacteristicDir = NULL;
+BLECharacteristic *pCharacteristicSpeed = NULL;
+
+#define BRAKE_PIN 27
+#define STOP_PIN 25
+#define DIR_PIN 35
+#define SPEED_PIN 32
+#define LED1_PIN 14
+
+String serverName = "ESP32_Skateboard_contoller";
+bool deviceConnected = false;
+
+volatile int flag_brake = 0;
+volatile int flag_stop = 0;
+volatile int flag_dir = 0;
 void IRAM_ATTR toggle_brake();
 void IRAM_ATTR toggle_stop();
 void IRAM_ATTR toggle_dir();
-uint8_t interval = 1000;
-unsigned long previousTime = 0;
-unsigned long currentTime;
-void setup() {
-  pinMode(27, INPUT);
-  pinMode(25, INPUT);
-  pinMode(35, INPUT);
-  pinMode(32, INPUT);
-  pinMode(14, OUTPUT);
-  attachInterrupt(27, toggle_brake, RISING);
-  attachInterrupt(25, toggle_stop, RISING);
-  attachInterrupt(35, toggle_dir, RISING);
-  Serial.begin(115200); // Initializing serial connection for debugging
-  BT.begin("ESP32_client", true); // Name of your Bluetooth Device and in master mode
-  Serial.println("Bluetooth Device is in master mode. Connecting to the host...");
-  connected = BT.connect(clientName);
-  if(connected) {
+
+const int PWM_FREQ = 19000;     
+const int PWM_RESOLUTION = 12; 
+const int MAX_DUTY_CYCLE = (int)(pow(2, PWM_RESOLUTION) - 1); 
+
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
     Serial.println("Connected Succesfully!");
-    digitalWrite(14, HIGH); 
-  } else {
-    while(!BT.connected(100)) {
-      Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
-      digitalWrite(14, LOW);
-      delay(100); 
-      digitalWrite(14, HIGH); 
-      delay(100); 
-    }
+    digitalWrite(LED1_PIN, HIGH); 
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    Serial.println("Device disconnected!!!"); 
   }
+};
+
+void setup() {
+  pinMode(BRAKE_PIN, INPUT);
+  pinMode(STOP_PIN, INPUT);
+  pinMode(DIR_PIN, INPUT);
+  pinMode(SPEED_PIN, INPUT);
+  pinMode(LED1_PIN, OUTPUT);
+  attachInterrupt(BRAKE_PIN, toggle_brake, RISING);
+  attachInterrupt(STOP_PIN, toggle_stop, RISING);
+  attachInterrupt(DIR_PIN, toggle_dir, RISING);
+
+  Serial.begin(115200);
+  Serial.println("BLE is intilized, waiting for connection.");
+
+  BLEDevice::init(serverName);
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristicBrake = pService->createCharacteristic(
+                                         BRAKE_CHAR_UUID,
+                                         BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
+                                       );
+  pCharacteristicStop = pService->createCharacteristic(
+                                         STOP_CHAR_UUID,
+                                         BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
+                                       );     
+  pCharacteristicDir = pService->createCharacteristic(
+                                         DIR_CHAR_UUID,
+                                         BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
+                                       );   
+  pCharacteristicSpeed = pService->createCharacteristic(
+                                         SPEED_CHAR_UUID,
+                                         BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
+                                       );  
+                                                                                                
+  pService->start();
+  pCharacteristicBrake->setValue((uint8_t *)&flag_brake, 1); 
+  pCharacteristicBrake->notify(); 
+  pCharacteristicStop->setValue((uint8_t *)&flag_stop, 1); 
+  pCharacteristicStop->notify();    
+  pCharacteristicDir->setValue((uint8_t *)&flag_dir, 1); 
+  pCharacteristicDir->notify();   
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristics defined! Now you can read it in your phone!");
 }
 
 void IRAM_ATTR toggle_brake(){
- if (digitalRead(27) == HIGH && flag == 0){
-    BT.write(97); // Send a in ASCII
-    flag = 1;
-    //delay(10);
-  }else if(digitalRead(27) == HIGH && flag == 1){
-    BT.write(98); // Send b in ASCII
-    flag = 0;
-    //delay(10);
+ if (digitalRead(BRAKE_PIN) == HIGH && flag_brake == 0){
+    flag_brake = 1;
+    pCharacteristicBrake->setValue((uint8_t *)&flag_brake, 1);
+    pCharacteristicBrake->notify();
+    
+    Serial.println("on");
+
+  }else if(digitalRead(BRAKE_PIN) == HIGH && flag_brake == 1){
+    flag_brake = 0;
+    Serial.println("off");
+    pCharacteristicBrake->setValue((uint8_t *)&flag_brake, 1); 
+    pCharacteristicBrake->notify();
   }
 }
 
 void IRAM_ATTR toggle_stop(){
- if (digitalRead(25) == HIGH && flag_stop == 0){
-    BT.write(99); // Send c in ASCII
+ if (digitalRead(STOP_PIN) == HIGH && flag_stop == 0){
     flag_stop = 1;
-    //delay(10);
-  }else if(digitalRead(25) == HIGH && flag_stop == 1){
-    BT.write(100); // Send d in ASCII
+    pCharacteristicStop->setValue((uint8_t *)&flag_stop, 1); 
+    pCharacteristicStop->notify();
+  }else if(digitalRead(STOP_PIN) == HIGH && flag_stop == 1){
     flag_stop = 0;
-    //delay(10);
+    pCharacteristicStop->setValue((uint8_t *)&flag_stop, 1); 
+    pCharacteristicStop->notify();
+
   }
 }
 
 void IRAM_ATTR toggle_dir(){
- if (digitalRead(35) == HIGH && flag_dir == 0){
-    BT.write(101); // Send e in ASCII
+ if (digitalRead(DIR_PIN) == HIGH && flag_dir == 0){
     flag_dir = 1;
-    //delay(10);
-  }else if(digitalRead(35) == HIGH && flag_dir == 1){
-    BT.write(102); // Send f in ASCII
+    pCharacteristicDir->setValue((uint8_t *)&flag_dir, 1); 
+    pCharacteristicDir->notify();
+  }else if(digitalRead(DIR_PIN) == HIGH && flag_dir == 1){
     flag_dir = 0;
-    //delay(10);
+    pCharacteristicDir->setValue((uint8_t *)&flag_dir, 1); 
+    pCharacteristicDir->notify();
   }
 }
 
 
 void speed(){
-  currentTime=millis();
-  if (currentTime - previousTime >= interval){
-    value = analogRead(32);
-    if (value < 97 || value > 102){ 
-      BT.write(value);
-    }
-    previousTime = currentTime;
-  }
+    int pot_value = analogRead(SPEED_PIN);
+    pot_value = map(pot_value, 0, 4095, 0, MAX_DUTY_CYCLE); 
+    Serial.print("Decimal:");
+    Serial.print(pot_value);
+    Serial.print("Hex:");
+    Serial.println(pot_value, HEX);
+
+    pCharacteristicSpeed->setValue((uint8_t *)&pot_value, 4); 
+    pCharacteristicSpeed->notify();
+    delay(50);
 }
 
 void loop() {
-  speed();
+  if (deviceConnected){
+    speed();
+  }else{
+    digitalWrite(LED1_PIN, LOW);
+    delay(100); 
+    digitalWrite(LED1_PIN, HIGH); 
+    delay(100); 
+    BLEDevice::startAdvertising();
+  }
 }
